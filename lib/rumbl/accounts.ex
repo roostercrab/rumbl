67cfa467d2debd211ed9881d8560f68bba50156a
roostercrab <jesse.shea@qixingdesign.com>
2020-01-07 09:34:42 -0800

    added schema

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -1,24 +1,11 @@
 defmodule Rumbl.Accounts do
-  @moduledoc """
-  The Accounts context
-  """
-  alias Rumbl.Accounts.User
+  use Ecto.Schema
+  import Ecto.Changeset
 
-  def list_users do
-    [
-      %User{id: "1", name: "Billy Bob", username: "Bill"},
-      %User{id: "2", name: "Trilly Trob", username: "Trill"},
-      %User{id: "3", name: "Silly Sob", username: "Sill"}
-    ]
-  end
+  schema "users" do
+    field :name, :string
+    field :username, :string
 
-  def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
-  end
-
-  def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    timestamps()
   end
 end
